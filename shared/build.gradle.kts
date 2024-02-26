13e10e2aef29f69c1e9b832b179852683cf7ab3b
Sridhar Mocherla <smocherla@brex.com>
2024-02-26 16:16:53 +0000

    Remove protobufs and fix cquery files

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,11 +1,8 @@
-import com.google.protobuf.gradle.id
-
 plugins {
     id("maven-publish")
     kotlin("jvm")
     id("kotlin-language-server.publishing-conventions")
     id("kotlin-language-server.kotlin-conventions")
-    id("com.google.protobuf") version "0.9.4"
 }
 
 repositories {
@@ -19,27 +16,7 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.exposed:exposed-core")
-    implementation("com.google.protobuf:protobuf-kotlin:3.21.2")
-    implementation("com.google.protobuf:protobuf-java-util:3.21.4")
     implementation("org.jetbrains.exposed:exposed-dao")
     testImplementation("org.hamcrest:hamcrest-all")
     testImplementation("junit:junit")
-    protobuf(files("src/main/protobuf/"))
-}
-
-protobuf {
-    protoc {
-        artifact = "com.google.protobuf:protoc:3.19.4"
-    }
-
-    // Enable Kotlin generation
-    generateProtoTasks {
-        all().forEach {
-            it.builtins {
-                id("kotlin")
-            }
-            it.sourceDirs
-
-        }
-    }
 }
